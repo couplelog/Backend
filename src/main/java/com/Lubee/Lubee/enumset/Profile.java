97ecff3be078c236a43438b8f10ad68baedece45
unknown <sunghyun1356@naver.com>
2024-07-24 05:00:53 +0900

    [mod] profile modify

--- src/main/java/com/Lubee/Lubee/enumset/Profile.java
+++ src/main/java/com/Lubee/Lubee/enumset/Profile.java
@@ -5,9 +5,14 @@ import lombok.Getter;
 @Getter
 public enum Profile {
     a("1"),
-    b("b"),
-    c("c"),
-    d("d");
+    b("2"),
+    c("3"),
+    d("4"),
+    e("5"),
+    f("6"),
+    g("7"),
+    h("8");
+
 
     private final String ProfileName;
 
