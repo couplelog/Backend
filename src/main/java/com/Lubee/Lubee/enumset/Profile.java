8d9ac8012e9b2e625060a54938f4d2cc4216ab32
parking0 <p980710@gmail.com>
2024-07-25 21:13:33 +0900

    [mod] profile format change

--- src/main/java/com/Lubee/Lubee/enumset/Profile.java
+++ src/main/java/com/Lubee/Lubee/enumset/Profile.java
@@ -4,14 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum Profile {
-    a("1"),
-    b("2"),
-    c("3"),
-    d("4"),
-    e("5"),
-    f("6"),
-    g("7"),
-    h("8");
+    a("a"),
+    b("b"),
+    c("c"),
+    d("d"),
+    e("e"),
+    f("f"),
+    g("g"),
+    h("h");
 
 
     private final String ProfileName;
