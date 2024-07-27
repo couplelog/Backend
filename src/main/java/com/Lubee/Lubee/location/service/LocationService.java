31be89206db36f71c8deffec3c98fb39e4945b14
parking0 <p980710@gmail.com>
2024-07-27 16:41:37 +0900

    [fix] change return value of location

--- src/main/java/com/Lubee/Lubee/location/service/LocationService.java
+++ src/main/java/com/Lubee/Lubee/location/service/LocationService.java
@@ -41,7 +41,8 @@ public class LocationService {
                 .collect(Collectors.toList());
 
         if(locationDtos.isEmpty()){
-            throw new RestApiException(ErrorType.LOCATION_NOT_FOUND);
+            return ResponseUtils.ok(null, ErrorResponse.builder().status(200).message("해당 키워드로 검색된 결과가 없습니다").build());
+            //throw new RestApiException(ErrorType.LOCATION_NOT_FOUND);
         }
 
         return ResponseUtils.ok(new LocationSearchResponse(locationDtos), ErrorResponse.builder().status(200).message("위치 검색 성공").build());
